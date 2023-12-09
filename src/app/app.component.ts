f1be08b7c21de1028a8184f63246e07b1b88b7f7
Kauã Felinto <136202483+KauaFelinto@users.noreply.github.com>
2023-12-09 12:53:43 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { CarrousselComponent } from "./carroussel/carroussel.component";
+import { CarrousselModule } from './carroussel/carroussel.module';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, CarrousselComponent, CarrousselModule]
 })
 export class AppComponent {
   title = 'website';
+
+  images = [
+    {
+      imageSrc:
+        'https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/3f283002b47c62b1e5d311c45f95c97e.jpe',
+      imageAlt: 'nature1',
+    },
+    {
+      imageSrc:
+        'https://s2-techtudo.glbimg.com/1Q76caEdN3BriFZ_l61VD5MJkYs=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/g/0/s4BJiLQGSb9USN3vTvAg/death-note-netflix.jpg',
+      imageAlt: 'nature2',
+    },
+    {
+      imageSrc:
+        'https://m.media-amazon.com/images/S/pv-target-images/78ebbfc64cc407b773d71b7ec63dbde78293f74f672bf1b10c5985e3c13e9a47.jpg',
+      imageAlt: 'person1',
+    },
+    {
+      imageSrc:
+        'https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/2e54344c7cc1675af721b4021222230f.jpe',
+      imageAlt: 'person2',
+    },
+  ]
 }
---
+++ src/app/carroussel/carroussel.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CarrousselModule } from './carroussel.module';
+import { CommonModule } from '@angular/common';
+
+interface carouselImage{
+  imageSrc: string;
+  imageAlt: string;
+};
+
+@Component({
+  selector: 'app-carroussel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carroussel.component.html',
+  styleUrl: './carroussel.component.css'
+})
+export class CarrousselComponent implements OnInit{
+  @Input() images: carouselImage[] = []
+  @Input() indicators = true;
+  @Input() controls = true;
+  @Input() autoSlide = false;
+  @Input() slideInterval = 1000;
+
+  selectedindex = 0; 
+
+  ngOnInit(): void {
+    if(this.autoSlide){
+      this.autoSlideImages();
+    }
+  }
+
+  autoSlideImages(): void {
+    setInterval(() => {
+      this.onNextClick();
+    }, this.slideInterval);
+  }
+
+  //set index image
+  selectImage(index: number): void {
+    this.selectedindex = index;
+  }
+
+  onPrevClick(): void{
+    if(this.selectedindex === 0){
+      this.selectedindex = this.images.length - 1;
+    } else{
+      this.selectedindex--;
+    }
+  }
+
+  onNextClick(): void {
+    if(this.selectedindex === this.images.length + 1){
+      this.selectedindex = 0;
+    } else{
+      this.selectedindex++;
+    }
+  }
+}
